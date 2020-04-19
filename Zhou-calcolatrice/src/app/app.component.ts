0849f7115d58d6de5f0c3e79e6a72e1f6ad616bd
Zhollo <zhou.lorenzo@iisgalvanimi.edu.it>
2020-04-19 12:31:43 +0000

    .ts

--- Zhou-calcolatrice/src/app/app.component.ts
+++ Zhou-calcolatrice/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Zhou-calcolatrice';
+
+}
+
+export class operazioni{
+  somma(){
+    var a = Number(document.getElementById("num1"));
+    var b = Number(document.getElementById("num2"));
+
+    var c = a+b;
+    console.log(c)
+  }
 }
